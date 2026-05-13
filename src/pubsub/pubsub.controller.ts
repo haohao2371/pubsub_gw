8faa7d60d251e56d6fbe699db4bfa58ff25d1749
haohao2371 <39752856+haohao2371@users.noreply.github.com>
2026-05-13 15:03:54 +0700

    Add Kafka payments event listener to pubsub controller

--- src/pubsub/pubsub.controller.ts
+++ src/pubsub/pubsub.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { EventPattern, Payload } from '@nestjs/microservices';
 import { ChannelDefinition } from './pubsub.constants';
 import { PublishMessageRequest, PublishedMessageResponse } from './pubsub.types';
 import { PubSubService } from './pubsub.service';
@@ -20,5 +21,10 @@ export class PubSubController {
   @Post('publish')
   publish(@Body() body: PublishMessageRequest): PublishedMessageResponse {
     return this.pubSubService.publish(body);
+  }
+
+  @EventPattern('payments')
+  handlePaymentsEvent(@Payload() message: unknown): void {
+    this.pubSubService.handleKafkaPaymentsEvent(message);
   }
 }
